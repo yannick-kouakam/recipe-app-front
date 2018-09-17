5501aae979327b1e30ec8e2f72a8e27f9a61e346
yannick-kouakam <ykouakam@gmail.com>
2018-09-17 23:04:03 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,30 @@
   <div id="app">
     <header-app/>
     <router-view/>
+    <footer-app/>
   </div>
 </template>
 
 <script>
   import HeaderApp from "./components/Header.vue";
-export default {
-  name: 'App',
-  components:{
-    HeaderApp
+  import FooterApp from "./components/Footer.vue";
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderApp,
+      FooterApp
+    }
   }
-}
 </script>
 
 <style>
+  html {
+    position: relative;
+    min-height: 100%;
+  }
 
+  body {
+    margin-bottom: 60px; /* Margin bottom by footer height */
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p class="float-right"><a href="#">Back to top</a></p>
+      <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style scoped>
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px; /* Set the fixed height of the footer here */
+    line-height: 60px; /* Vertically center the text there */
+    background-color: #f5f5f5;
+  }
+
+  .container {
+    width: auto;
+    max-width: 680px;
+    padding: 0 15px;
+  }
+</style>
